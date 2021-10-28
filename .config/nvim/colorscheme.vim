5b13442845d3399fab73733b2f3cbe5d42fd5e2c
zodd18 <josiesala@gmail.com>
2021-10-28 14:16:02 +0200

    Minor changes

--- .config/nvim/colorscheme.vim
+++ .config/nvim/colorscheme.vim
@@ -3,6 +3,7 @@
 
 set termguicolors
 let g:airline_powerline_fonts = 1                                               
-colorscheme forest-night
+let g:everforest_background = 'soft'
+colorscheme everforest
 
 " }
--- .config/rofi/launch-rofi.sh
+++ .config/rofi/launch-rofi.sh
@@ -1,1 +1,1 @@
-rofi -font "Iosevka Nerd Font Mono 9" -show drun -show-icons -width 25 -lines 11.5 -config ~/.config/rofi/config
+rofi -font "Iosevka Bold 11" -show drun -show-icons -width 25 -lines 11 -config ~/.config/rofi/config
